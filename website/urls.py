3669fb7c23cd2bdc7b6f5775cc10dd06a28fb994
Marcello Antony <marcello.antonymr@gmail.com>
2025-01-24 18:26:47 -0300

    Alterações Cidades

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('services/', views.services, name='services'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('cities/', views.cities_view, name='cities'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
+import csv
+from django.http import JsonResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -11,3 +15,40 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def cities(request):
+    return render(request, 'cities.html')
+
+
+def cities_view(request):
+    csv_file_path = os.path.join(settings.BASE_DIR, 'cidades.csv')
+    cities = []
+
+    try:
+        # Leia o arquivo CSV com codificação adequada
+        with open(csv_file_path, 'r', encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for row in reader:
+                cidade = row.get('cidade') or row.get('Cidade')
+                atendimento = row.get('atendimento') or row.get('Atendimento')
+                if cidade and atendimento:
+                    cities.append((cidade, atendimento))
+    except FileNotFoundError:
+        print("Erro: Arquivo cidades.csv não encontrado.")
+    except UnicodeDecodeError as e:
+        print(f"Erro de decodificação: {e}")
+    except KeyError as e:
+        print(f"Erro: Chave {e} não encontrada no CSV.")
+
+    # Filtrar as cidades com base na busca
+    query = request.GET.get('q', '').lower()
+    filtered_cities = [(cidade, atendimento) for cidade, atendimento in cities if query in cidade.lower()] if query else cities
+
+    # Se for uma requisição AJAX, retorne JSON
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        return JsonResponse({'cidades': filtered_cities[:20]})
+
+    context = {
+        'cidades': cities[:-1],  # Mostrar apenas 5 cidades inicialmente
+    }
+    return render(request, 'cities.html', context)
